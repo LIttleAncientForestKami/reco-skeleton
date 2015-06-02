25e209d74f011f46f88ec8aa213bb9235103d9cf
LittleAncientForestKami <Tomasz.Borek@gmail.com>
2015-06-03 01:42:55 +0200

    App and Config classes, base of it all
    A tool-maker succeeds as, and only as, the users of his tool succeed with his aid
    		 -- Fred Brooks

---
+++ src/main/java/edu/lafk/recoskeleton/RecoSkeletonApp.java
@@ -0,0 +1,19 @@
+package edu.lafk.recoskeleton;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
+/**
+ * TODO: Write here what this does?
+ * <HR>
+ * Created 03.06.15 01:35
+ *
+ * @author LAFK (@LAFK_pl || LIttleAncientForestKami@Github)
+ */
+public class RecoSkeletonApp extends Application<RecoSkeletonAppConfig> {
+
+    @Override
+    public void run(RecoSkeletonAppConfig recoSkeletonAppConfig, Environment environment) throws Exception {
+
+    }
+}
---
+++ src/main/java/edu/lafk/recoskeleton/RecoSkeletonAppConfig.java
@@ -0,0 +1,13 @@
+package edu.lafk.recoskeleton;
+
+import io.dropwizard.Configuration;
+
+/**
+ * TODO: Write here what this does?
+ * <HR>
+ * Created 03.06.15 01:37
+ *
+ * @author LAFK (@LAFK_pl || LIttleAncientForestKami@Github)
+ */
+public class RecoSkeletonAppConfig extends Configuration {
+}
