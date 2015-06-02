c5c7fd7fe64df719c596cfb36610f8f831f9893c
LittleAncientForestKami <Tomasz.Borek@gmail.com>
2015-06-03 01:48:26 +0200

    Basic config
    Computers are useless.  They can only give you answers.
    	    -- Pablo Picasso

--- src/main/java/edu/lafk/recoskeleton/RecoSkeletonAppConfig.java
+++ src/main/java/edu/lafk/recoskeleton/RecoSkeletonAppConfig.java
@@ -1,6 +1,8 @@
 package edu.lafk.recoskeleton;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * TODO: Write here what this does?
@@ -10,4 +12,31 @@ import io.dropwizard.Configuration;
  * @author LAFK (@LAFK_pl || LIttleAncientForestKami@Github)
  */
 public class RecoSkeletonAppConfig extends Configuration {
+
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String recommendations = "random or offer of the day";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getRecommendations() {
+        return recommendations;
+    }
+
+    @JsonProperty
+    public void setRecommendations(String name) {
+        this.recommendations = name;
+    }
+
 }
